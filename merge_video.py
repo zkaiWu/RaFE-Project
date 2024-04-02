4e405fd5119a0cff93780bcd7767da8cfa59bd5a
wzkwzk1998 <869693441@qq.com>
2024-04-02 17:15:24 +0800

    add teaser video and mesh

--- merge_video.py
+++ merge_video.py
@@ -1,8 +1,8 @@
 import imageio
 import numpy as np
 
-video1 = imageio.get_reader('static\mesh_video\\ficus_coarse.mkv')
-video2 = imageio.get_reader('static\mesh_video\\ficus_fine.mkv')
+video1 = imageio.get_reader('static\mesh_video\\lego_coarse.mkv')
+video2 = imageio.get_reader('static\mesh_video\\lego_fine.mkv')
 
 
 fps = video1.get_meta_data()['fps']
@@ -10,7 +10,7 @@ frame_width = video1.get_meta_data()['source_size'][0]
 frame_height = video1.get_meta_data()['source_size'][1]
 fps = video1.get_meta_data()['fps']
 
-output_file = 'static\mesh_video\\ficus_coarse_fine.mp4'
+output_file = 'static\mesh_video\\lego_coarse_fine.mp4'
 writer = imageio.get_writer(output_file, fps=fps)
 
 for i, (frame1, frame2) in enumerate(zip(video1, video2)):
